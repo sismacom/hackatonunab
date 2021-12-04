f854eb5d3629bf2fed8f8da0c167fb09e7d20f41
sismacom <tomasmartinezarrieta@gmail.com>
2021-12-03 20:58:30 -0500

    Postman

--- Spring Boot Project/src/main/java/com/ejemplo/tiendaalamano/controller/MarcaController.java
+++ Spring Boot Project/src/main/java/com/ejemplo/tiendaalamano/controller/MarcaController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,13 +42,13 @@ public class MarcaController {
 
     @PostMapping("/save")
     @ResponseStatus(HttpStatus.CREATED)
-    public Marca save(Marca marca) {
+    public Marca save(@RequestBody Marca marca) {
         return marcaServ.save(marca);
     }
 
     @DeleteMapping("/borrar")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteById(Long Id) {
+    public void deleteById(@PathVariable Long Id) {
         marcaServ.deleteById(Id);
     }
 
